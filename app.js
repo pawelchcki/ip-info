98feb238ae25f887ba06287e63cff15938f7b609
Pawel Chojnacki <pawelchcki@gmail.com>
2018-03-19 01:38:16 +0100

    This time I remembered to save the progress :)

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const { fork } = require('child_process');
+const { Observable } = require('rxjs');
+const config = require('config');
+const Git = require('nodegit');
+
 
 var current_worker = null;
 var worker_no = 0;
 
 function build_worker() {
-  const worker = fork('grpc_worker.js', ['blocklist-ipsets'], { silent: false, execArgv: [] });
+  const worker = fork('grpc_worker.js', { silent: false, execArgv: [] });
   worker.number = worker_no;
   worker_no++;
-  
+
   worker.on('message', (data) => {
     console.log(`worker(${worker.number})`, data);
   });
@@ -20,27 +24,69 @@ function build_worker() {
 }
 
 function worker_ready(worker, callback) {
-  worker.on('message', (data) => { 
-    if (data === 'ready'){
+  worker.on('message', (data) => {
+    if (data === 'ready') {
       callback(worker);
     }
   });
 }
 
-function simulateUpdate(time){
-  setTimeout(()=>{
+const pollingInterval = config.get('blacklist.polling_interval');
+const repositoryUrl = config.get('blacklist.repository_url');
+Observable.interval(pollingInterval);
+
+const tmp = require('tmp');
+const fs = require('fs');
+const dir = tmp.dirSync();
+
+const repositoryStorage = config.get('blacklist.repository_storage');
+if (!repositoryStorage) {
+  console.log('Repository strage not configured');
+  process.exit(1);
+}
+
+
+const cloneRepo = Observable.defer(() => {
+  console.log(`Cloning ${repositoryUrl} to ${repositoryStorage}`);
+  return Git.Clone(repositoryUrl, repositoryStorage);
+});
+
+var currentOid = null;
+
+Observable.defer(() => {
+  console.log(`Initializing repository ${repositoryStorage}`);
+  return Git.Repository.open(repositoryStorage);
+})
+  .catch(() => cloneRepo)
+  .do(() => console.log('Done'))
+  .subscribe((repo) => {
+    Observable.from(repo.head())
+      .map(ref => ref.target())
+      .do((oid) => currentOid = oid)
+      .subscribe((oldOid) => {
+        console.log(`Starting polling upstream repo every ${pollingInterval}ms`);
+        Observable.interval(pollingInterval)
+          .flatMap((i)=>{
+            return repo.fetch('origin');
+          }).subscribe(console.log);
+      });
+  });
+
+
+function simulateUpdate(time) {
+  setTimeout(() => {
     console.log('source is updated - reload worker');
-    
+
     worker_ready(build_worker(), (worker) => {
-      if (current_worker !== null){
+      if (current_worker !== null) {
         current_worker.kill();
       }
-      
+
       current_worker = worker;
       simulateUpdate(100);
     });
-    
+
   }, time);
 }
 
-simulateUpdate(1);
+// simulateUpdate(1);
